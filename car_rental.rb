dc3f218b449110d4116f84f91fa1676e33d7b249
PHedro <pedrohenrique.reis@gmail.com>
2020-08-16 19:03:23 +0100

    refactory and additional json_statement methods

--- car_rental.rb
+++ car_rental.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'json'
+
 class Car
   SALOON = 0
   SUV = 1
@@ -12,6 +14,27 @@ class Car
     @title = title
     @style = style
   end
+
+  def base_rate
+    style == Car::SUV ? 30 : 15
+  end
+
+  def initial_rate
+    rate = 0
+    if [Car::HATCHBACK, Car::SALOON].include? style
+      rate = style == Car::HATCHBACK ? 15 : 20
+    end
+    rate
+  end
+
+  def base_days_discount
+    days = 0
+    if [Car::HATCHBACK, Car::SALOON].include? style
+      days = style == Car::HATCHBACK ? 3 : 2
+    end
+    days
+  end
+
 end
 
 class Rental
@@ -22,6 +45,26 @@ class Rental
     @days_rented = days_rented
 
     raise 'Error: days_rented invalid' if @days_rented <= 0
+  end
+
+  def amount
+    this_amount = car.initial_rate
+    this_amount += (days_rented - car.base_days_discount) * car.base_rate if days_rented > car.base_days_discount
+    this_amount
+  end
+
+  def bonus_points
+    bonus_points = amount.negative? ? -10 : 1
+    bonus_points += 1 if car.style == Car::SUV && days_rented > 1
+    bonus_points
+  end
+
+  def to_s
+    car.title.to_s + ',' + amount.to_s + "\n"
+  end
+
+  def car_amount
+    [car.title.to_s, amount]
   end
 end
 
@@ -37,34 +80,72 @@ class Driver
     @rentals << rental
   end
 
+  def total_bonus
+    bonus_points = 0
+    @rentals.each do |r|
+      bonus_points += r.bonus_points
+    end
+    bonus_points
+  end
+
+  def total_owed
+    total = 0
+    @rentals.each do |r|
+      total += r.amount
+    end
+    total
+  end
+
   def statement
     total = 0
     bonus_points = 0
     result = "Car rental record for #{@name}\n"
     @rentals.each do |r|
-      this_amount = 0
-      case r.car.style
-      when Car::SUV
-        this_amount += r.days_rented * 30
-      when Car::HATCHBACK
-        this_amount += 15
-        this_amount += (r.days_rented - 3) * 15 if r.days_rented > 3
-      when Car::SALOON
-        this_amount += 20
-        this_amount += (r.days_rented - 2) * 15 if r.days_rented > 2
-      end
-
-      bonus_points -= 10 if this_amount.negative?
-
-      bonus_points += 1
-      bonus_points += 1 if r.car.style == Car::SUV && r.days_rented > 1
-
-      result += r.car.title.to_s + ',' + this_amount.to_s + "\n"
+      this_amount = r.amount
       total += this_amount
+      bonus_points += r.bonus_points
+
+      result += r.to_s
     end
 
     result += 'Amount owed is €' + total.to_s + "\n"
     result += 'Earned bonus points: ' + bonus_points.to_s
+  end
+
+  def statements
+    result = []
+    @rentals.each do |r|
+      result << r.car_amount
+    end
     result
+  end
+
+  def json_statement
+    statement_hash = {
+        :name => name,
+        :bonus_points => total_bonus,
+        :amount_owed => total_owed,
+        :statements => statements
+    }
+    JSON.generate(statement_hash)
+  end
+
+  def json_statement2
+    # second option cycling only once on the rentals list
+    bonus_total = 0
+    owed_total = 0
+    statements_array = []
+    @rentals.each do |r|
+      owed_total += r.amount
+      bonus_total += r.bonus_points
+      statements_array << r.car_amount
+    end
+    statement_hash = {
+        :name => name,
+        :bonus_points => bonus_total,
+        :amount_owed => owed_total,
+        :statements => statements_array
+    }
+    JSON.generate(statement_hash)
   end
 end
--- tests/tests.rb
+++ tests/tests.rb
@@ -10,6 +10,52 @@ class CarTest < Test::Unit::TestCase
     assert_equal 'car1', car_instance.title
     assert_equal 0, car_instance.style
   end
+
+  def test_base_rate_suv
+    car_instance = Car.new('car1', Car::SUV)
+    assert_equal 30, car_instance.base_rate
+  end
+
+  def test_base_rate_saloon
+    car_instance = Car.new('car1', Car::SALOON)
+    assert_equal 15, car_instance.base_rate
+  end
+
+  def test_base_rate_hatchback
+    car_instance = Car.new('car1', Car::HATCHBACK)
+    assert_equal 15, car_instance.base_rate
+  end
+
+  def test_initial_rate_suv
+    car_instance = Car.new('car1', Car::SUV)
+    assert_equal 0, car_instance.initial_rate
+  end
+
+  def test_initial_rate_saloon
+    car_instance = Car.new('car1', Car::SALOON)
+    assert_equal 20, car_instance.initial_rate
+  end
+
+  def test_initial_rate_hatchback
+    car_instance = Car.new('car1', Car::HATCHBACK)
+    assert_equal 15, car_instance.initial_rate
+  end
+
+  def test_base_days_suv
+    car_instance = Car.new('car1', Car::SUV)
+    assert_equal 0, car_instance.base_days_discount
+  end
+
+  def test_base_days_saloon
+    car_instance = Car.new('car1', Car::SALOON)
+    assert_equal 2, car_instance.base_days_discount
+  end
+
+  def test_base_days_hatchback
+    car_instance = Car.new('car1', Car::HATCHBACK)
+    assert_equal 3, car_instance.base_days_discount
+  end
+
 end
 
 class RentalTest < Test::Unit::TestCase
@@ -27,6 +73,78 @@ class RentalTest < Test::Unit::TestCase
     assert_raise do
       Rental.new(car_instance, 0)
     end
+  end
+
+  def test_rental_amount
+    car_instance = Car.new('car1', Car::SUV)
+    rental_instance = Rental.new(car_instance, 1)
+    assert_equal 30, rental_instance.amount
+  end
+
+  def test_rental_amount_suv_month
+    car_instance = Car.new('car1', Car::SUV)
+    rental_instance = Rental.new(car_instance, 30)
+    assert_equal 900, rental_instance.amount
+  end
+
+  def test_rental_amount_hatch_month
+    car_instance = Car.new('car1', Car::HATCHBACK)
+    rental_instance = Rental.new(car_instance, 30)
+    assert_equal 420, rental_instance.amount
+  end
+
+  def test_rental_amount_hatchback
+    car_instance = Car.new('car1', Car::HATCHBACK)
+    rental_instance = Rental.new(car_instance, 1)
+    assert_equal 15, rental_instance.amount
+  end
+
+  def test_rental_saloon_month
+    car_instance = Car.new('car1', Car::SALOON)
+    rental_instance = Rental.new(car_instance, 30)
+    assert_equal 440, rental_instance.amount
+  end
+
+  def test_rental_amount_saloon
+    car_instance = Car.new('car1', Car::SALOON)
+    rental_instance = Rental.new(car_instance, 1)
+    assert_equal 20, rental_instance.amount
+  end
+
+  def test_bonus_points
+    car_instance = Car.new('car1', Car::SUV)
+    rental_instance = Rental.new(car_instance, 1)
+    assert_equal 1, rental_instance.bonus_points
+  end
+
+  def test_bonus_points_2_days
+    car_instance = Car.new('car1', Car::SUV)
+    rental_instance = Rental.new(car_instance, 2)
+    assert_equal 2, rental_instance.bonus_points
+  end
+
+  def test_bonus_points_saloon
+    car_instance = Car.new('car1', Car::SALOON)
+    rental_instance = Rental.new(car_instance, 1)
+    assert_equal 1, rental_instance.bonus_points
+  end
+
+  def test_bonus_saloon_2_days
+    car_instance = Car.new('car1', Car::SALOON)
+    rental_instance = Rental.new(car_instance, 2)
+    assert_equal 1, rental_instance.bonus_points
+  end
+
+  def test_to_s
+    car_instance = Car.new('car1', Car::SALOON)
+    rental_instance = Rental.new(car_instance, 1)
+    assert_equal "car1,20\n", rental_instance.to_s
+  end
+
+  def test_car_amount
+    car_instance = Car.new('car1', Car::SALOON)
+    rental_instance = Rental.new(car_instance, 1)
+    assert_equal ['car1', 20], rental_instance.car_amount
   end
 end
 
@@ -107,5 +225,63 @@ class DriverTest < Test::Unit::TestCase
     driver_instance.add_rental rental_instance2
     expected = "Car rental record for John\ncar1,20\ncar2,20\nAmount owed is €40\nEarned bonus points: 2"
     assert_equal expected, driver_instance.statement
+  end
+
+  def test_statemens
+    car_instance = Car.new('car1', Car::SALOON)
+    car_instance2 = Car.new('car2', Car::SALOON)
+    rental_instance = Rental.new(car_instance, 1)
+    rental_instance2 = Rental.new(car_instance2, 1)
+    driver_instance = Driver.new('John')
+    driver_instance.add_rental rental_instance
+    driver_instance.add_rental rental_instance2
+    expected = [['car1', 20], ['car2', 20]]
+    assert_equal expected, driver_instance.statements
+  end
+
+  def test_total_bonus
+    car_instance = Car.new('car1', Car::SALOON)
+    car_instance2 = Car.new('car2', Car::SALOON)
+    rental_instance = Rental.new(car_instance, 1)
+    rental_instance2 = Rental.new(car_instance2, 1)
+    driver_instance = Driver.new('John')
+    driver_instance.add_rental rental_instance
+    driver_instance.add_rental rental_instance2
+    assert_equal 2, driver_instance.total_bonus
+  end
+
+  def test_total_owed
+    car_instance = Car.new('car1', Car::SALOON)
+    car_instance2 = Car.new('car2', Car::SALOON)
+    rental_instance = Rental.new(car_instance, 1)
+    rental_instance2 = Rental.new(car_instance2, 1)
+    driver_instance = Driver.new('John')
+    driver_instance.add_rental rental_instance
+    driver_instance.add_rental rental_instance2
+    assert_equal 40, driver_instance.total_owed
+  end
+
+  def test_json_statement
+    car_instance = Car.new('car1', Car::SALOON)
+    car_instance2 = Car.new('car2', Car::SALOON)
+    rental_instance = Rental.new(car_instance, 1)
+    rental_instance2 = Rental.new(car_instance2, 1)
+    driver_instance = Driver.new('John')
+    driver_instance.add_rental rental_instance
+    driver_instance.add_rental rental_instance2
+    expected = "{\"name\":\"John\",\"bonus_points\":2,\"amount_owed\":40,\"statements\":[[\"car1\",20],[\"car2\",20]]}"
+    assert_equal expected, driver_instance.json_statement
+  end
+
+  def test_json_statement2
+    car_instance = Car.new('car1', Car::SALOON)
+    car_instance2 = Car.new('car2', Car::SALOON)
+    rental_instance = Rental.new(car_instance, 1)
+    rental_instance2 = Rental.new(car_instance2, 1)
+    driver_instance = Driver.new('John')
+    driver_instance.add_rental rental_instance
+    driver_instance.add_rental rental_instance2
+    expected = "{\"name\":\"John\",\"bonus_points\":2,\"amount_owed\":40,\"statements\":[[\"car1\",20],[\"car2\",20]]}"
+    assert_equal expected, driver_instance.json_statement2
   end
 end
